08755dc87e51567516891ed500e0c901466c8d74
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-14 12:20:36 +0100

    Added Movie & genre query, Edited rating query

--- SQL_Script.sql
+++ SQL_Script.sql
@@ -2,16 +2,23 @@ mysql -u root -p
 show databases;
 use movielens;
 
+-- Select titles that start with T
 SELECT title
     FROM movies
     WHERE title LIKE 'T%'
     ORDER BY title;
 
+-- Get list of movies and their associated genre
+
+SELECT movies.title, genres.name
+    FROM movies, genres
+    ORDER BY movies.title;
+
 -- Still currently processing the below query,
 -- the ratings table is big and my laptop is not happy
 
-SELECT movies.title, genres.name, ratings.rating
+SELECT movies.title, genres.name
     FROM movies, genres, ratings
-    WHERE genres.name = 'Comedy' AND ratings.rating >= 4
+    WHERE genres.name = 'Comedy' AND ratings.rating = 5
     ORDER BY movies.title;
 
