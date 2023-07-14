1e4cb8f3c2fc659abadad7d6bb56795e5a1ae57c
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-14 14:29:14 +0100

    Create GameshopDB

---
+++ Gameshopdb.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE gameshopDB
+
+use gameshopDB
+
+CREATE TABLE Customers (ID INT PRIMARY KEY, Email VARCHAR(200),Phone INT)
+CREATE TABLE Games (ID INT PRIMARY KEY, Title VARCHAR(200), R_Date DATE,
+InStock INT);
+CREATE TABLE orders( ID INT PRIMARY KEY, OrderNumber INT, CustomerID int, ProductID INT, FOREIGN KEY (CustomerID) REFERENCES customers(ID), FOREIGN KEY (ProductID) REFERENCES games(ID), OrderDate DATE);
+
+ALTER TABLE Customers
+    ADD ProductID
+
+ALTER TABLE Customers
+    ADD FORIENG (ProductID) REFERENCES games(ID);
