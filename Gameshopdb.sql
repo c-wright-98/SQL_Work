676bc67e139e60bb86736a0e726dfb44d1feb88c
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-14 14:30:25 +0100

    Changed use to USE

--- Gameshopdb.sql
+++ Gameshopdb.sql
@@ -1,14 +1,29 @@
-CREATE DATABASE gameshopDB
+CREATE DATABASE gameshopDB;
 
-use gameshopDB
+USE gameshopDB;
 
-CREATE TABLE Customers (ID INT PRIMARY KEY, Email VARCHAR(200),Phone INT)
-CREATE TABLE Games (ID INT PRIMARY KEY, Title VARCHAR(200), R_Date DATE,
-InStock INT);
-CREATE TABLE orders( ID INT PRIMARY KEY, OrderNumber INT, CustomerID int, ProductID INT, FOREIGN KEY (CustomerID) REFERENCES customers(ID), FOREIGN KEY (ProductID) REFERENCES games(ID), OrderDate DATE);
+CREATE TABLE Customers (
+    ID INT PRIMARY KEY,
+    Email VARCHAR(200),
+    Phone INT);
+
+CREATE TABLE Games (
+    ID INT PRIMARY KEY,
+    Title VARCHAR(200),
+    R_Date DATE,
+    InStock INT);
+
+CREATE TABLE orders(
+    ID INT PRIMARY KEY,
+    OrderNumber INT,
+    CustomerID int,
+    ProductID INT,
+    FOREIGN KEY (CustomerID) REFERENCES customers(ID),
+    FOREIGN KEY (ProductID) REFERENCES games(ID),
+    OrderDate DATE);
 
 ALTER TABLE Customers
-    ADD ProductID
+    ADD ProductID;
 
 ALTER TABLE Customers
-    ADD FORIENG (ProductID) REFERENCES games(ID);
+    ADD FOREIGN KEY (ProductID) REFERENCES games(ID);
